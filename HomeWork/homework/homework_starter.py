c1b91b3accf367834e6b5427a7e824b18f0c05c4
mr_marco <marco_meier2210@hotmail.com>
2021-11-23 14:17:53 +0100

    refactoring

---
+++ HomeWork/homework/homework_starter.py
@@ -0,0 +1,8 @@
+import homework.gradient_descent as gradient_descent
+import matplotlib.pyplot as plt
+
+
+def start_homework_gradient_descent():
+    plt.figure(figsize=(12, 8))
+    gradient_descent.start()
+    plt.show()
---
+++ HomeWork/main.py
@@ -0,0 +1,4 @@
+import homework.homework_starter as starter
+
+if __name__ == '__main__':
+    starter.start_homework_gradient_descent()

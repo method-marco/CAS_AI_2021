ae959748125aa5aaa1563fd36baeadf5c85b0982
mr_marco <marco_meier2210@hotmail.com>
2022-01-18 15:46:41 +0100

    basic implementation for step method

--- HomeWork/homework/reinforcement_learning/enivronment/TradingEnv.py
+++ HomeWork/homework/reinforcement_learning/enivronment/TradingEnv.py
@@ -16,14 +16,15 @@ class TradingEnv(gym.Env):
 
         self.current_idx = 0
         self.stocks = ['AAPL', 'MSFT', 'NFLX', 'AMZN'] # target
-        self.stocks_adj_close_names = [stock + '_Adj_Close' for stock in self.stocks] # Name of columns df
+        # self.stocks_adj_close_names = [stock + '_Adj_Close' for stock in self.stocks]
 
         self.initial_cash = initial_cash
         self.cash = 0
         self.portfolio_value = 0
         self.stock_values = np.zeros(len(self.stocks))
 
-        self.states = df_data.loc[:, ~df_data.columns.isin(self.stocks_adj_close_names)].to_numpy()
+        self.states = df_data.loc[:, ~df_data.columns.isin(self.stocks)].to_numpy()
+        self.n_states = len(self.states)
 
     def reset(self):
         self.current_idx = 0
@@ -36,6 +37,26 @@ class TradingEnv(gym.Env):
         return next_state
 
     def step(self, actions):
-        pass
+        reward = 0
+
+        if self.current_idx >= self.n_states:
+            raise Exception('Episode already done')
+
+        # state transistion
+        done = (self.current_idx == self.n_states-1)
+
+        # TODO: apply action
+
+        self.current_idx += 1
+
+        if not done:
+            # TODO: compute reward
+            next_state = self.states[self.current_idx]
+        else:
+            next_state = None
+            reward = 0
+
+        return next_state, reward, done
+
 
 
